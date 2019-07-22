93630cc0403926f467a3b21aee7acf6faf428d4a
Andrew Lucero <andrew.lucero@mastercard.com>
2019-07-22 13:10:55 -0400

    Removed extra import statement

--- src/test/java/mastercardsend/api/disbursements/DisbursementAccountInfoAPITest.java
+++ src/test/java/mastercardsend/api/disbursements/DisbursementAccountInfoAPITest.java
@@ -16,7 +16,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.io.IOException;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
